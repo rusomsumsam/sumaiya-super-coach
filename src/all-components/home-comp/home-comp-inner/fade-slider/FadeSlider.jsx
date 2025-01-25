fe6ba63da81e46fe1245550fd333ca682210a481
sumaiya <you@example.com>
2025-01-26 00:17:43 +0600

    Slider Update Some Code

--- src/all-components/home-comp/home-comp-inner/fade-slider/FadeSlider.jsx
+++ src/all-components/home-comp/home-comp-inner/fade-slider/FadeSlider.jsx
@@ -5,18 +5,20 @@ import '../fade-slider/FadeSlider.css';
 
 const FadeSlider = () => {
     const settings = {
-        dots: true,
+        dots: false,
         fade: true,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        waitForAnimate: false
-        
+        waitForAnimate: false,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
     };
     return (
         <div>
-            <div className="w-[80%] mx-auto">
+            <div className="w-[80%] mx-auto slide_main">
                 <Slider {...settings}>
                     <div className="first_slide"></div>
                     <div className="second_slide"></div>
